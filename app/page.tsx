12a87ed9a91595e55a08d86cf524aea7541603e3
Joao Montanholi <joao.montanholi@akurates-solutions.com>
2024-03-13 15:39:53 +0000

    Reached checkpoint on auth0 tutorial

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       </div>
       <div className="w-1/4 flex flex-col items-center justify-center gap-5 text-center">
         <p>Already have an account?</p>
-        <Link href='/login' className="hover:text-orange-600 border-2 hover:border-orange-300 py-2 px-5 rounded-md transition duration-250">
+        <Link href='/api/auth/login' className="hover:text-orange-600 border-2 hover:border-orange-300 py-2 px-5 rounded-md transition duration-250">
           Log In
         </Link>
       </div>
